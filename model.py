f05e085338c9daac3329f8363e560372e3d0cf2c
Yoav Ram <yoavram+github@gmail.com>
2012-11-22 11:47:49 +0200

    added fitness functions and genome to int functions for multiple locus model

--- model.py
+++ model.py
@@ -3,7 +3,8 @@ import numpy.random as npr
 
 
 pop_size = 10**6
-num_classes = 10**3
+num_classes = 1000
+num_loci = 1000
 s = 0.01
 mu = 0.003
 
@@ -15,11 +16,25 @@ def create_mutation_free_population():
 	population[0] = pop_size
 	return population
 
-def create_fitness():
+def create_fitness_by_mutational_load():
     return np.array( [ (1-s)**x for x in range(num_classes) ] )
 
 def create_muation_rates():
 	return np.array( [mu]*num_classes )
+
+def create_target_genome():
+	return np.array(zeros(num_loci), dtype=np.uint8)
+
+def hamming_fitness_genome(genome, target_genome):
+	return (genome != target_genome).sum()
+
+
+def hamming_fitness_genomes(genomes, target_genome):
+    return np.apply_along_axis(hamming_fitness_genome, 1, genomes, target_genome) 
+
+def genome_to_int(genome):
+	i = np.arange(genome.shape[0])
+	return int((2.**i*genome).sum())
 
 def drift(population):
 	pop_size = np.float32(population.sum())
--- model_c.pyx
+++ model_c.pyx
@@ -12,3 +12,13 @@ def mutation_by_mutation_load(np.ndarray[long, ndim=1, negative_indices=False] p
         population[i] -= mutations[i]
         population[i+1] += mutations[i]   
     return population
+
+@cython.boundscheck(False)
+def hamming_fitness_genomes(np.ndarray[int, ndim=2, negative_indices=False] genomes, np.ndarray[int, ndim=1, negative_indices=False] target_genome):
+	'''this is slightly faster than iterating without cython and twice as fast as using np.apply_along_axis'''
+    cdef np.ndarray[int, ndim=1, negative_indices=False] hamming_fitness 
+    hamming_fitness = np.zeros(genomes.shape[0], dtype=np.int)
+    cdef Py_ssize_t i
+    for i in range(genomes.shape[0]):
+        hamming_fitness[i] = (target_genome!=genomes[i,:]).sum()
+    return hamming_fitness
