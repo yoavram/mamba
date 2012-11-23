485ddfa97536b3cb269f46c288ca5532be81f8f1
Yoav Ram <yoavram+github@gmail.com>
2012-11-23 11:16:44 +0200

    incremental work

--- model_c.pyx
+++ model_c.pyx
@@ -8,22 +8,36 @@ cimport cython
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def mutation_by_mutation_load(np.ndarray[long, ndim=1] population, np.ndarray[double, ndim=1] mutation_rates):
-    cdef np.ndarray[long, ndim=1, negative_indices=False] mutations
-    mutations = npr.poisson(population*mutation_rates)
-    cdef Py_ssize_t i
-    for i in range(population.shape[0]-1):
-        population[i] -= mutations[i]
-        population[i+1] += mutations[i]   
-    return population
+	cdef np.ndarray[long, ndim=1, negative_indices=False] mutations
+	mutations = npr.poisson(population*mutation_rates)
+	cdef Py_ssize_t i
+	for i in range(population.shape[0]-1):
+		population[i] -= mutations[i]
+		population[i+1] += mutations[i]   
+		return population
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def hamming_fitness_genomes(np.ndarray[int, ndim=2] genomes, np.ndarray[int, ndim=1] target_genome, double s):
-	'''this is slightly faster than iterating without cython and twice as fast as using np.apply_along_axis'''
-    cdef np.ndarray[double, ndim=1] hamming_fitness 
-    hamming_fitness = np.zeros(genomes.shape[0], dtype=np.int)
-    cdef Py_ssize_t i
-    for i in range(genomes.shape[0]):
-    	# TODO clib power?
-        hamming_fitness[i] = s**((target_genome!=genomes[i,:]).sum()) 
-    return hamming_fitness
+	cdef np.ndarray[double, ndim=1] hamming_fitness 
+	hamming_fitness = np.zeros(genomes.shape[0], dtype=np.int)
+	cdef Py_ssize_t i
+	for i in range(genomes.shape[0]):
+		# TODO clib power? also [,:] isn't fast like i,j
+		hamming_fitness[i] = s ** ((target_genome != genomes[i,:]).sum()) 
+	return hamming_fitness
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def genomes_to_nums(genomes):
+	cdef np.ndarray[double, ndim=1] nums
+	cdef np.ndarray[double, ndim=1] i
+	cdef double num_loci_f 
+	num_loci_f = float(genomes.shape[1])
+	nums = np.zeros(genomes.shape[0], dtype=np.float)
+	cdef Py_ssize_t j
+	for j in range(genomes.shape[0]):
+		i = np.arange(num_loci_f)
+		nums[j] = (2. ** i * genomes[j,:]).sum()
+	return nums
