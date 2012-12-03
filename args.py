946fbdd79a9b7955809f7f122d932072865f87b4
Yoav Ram <yoavram+github@gmail.com>
2012-12-03 10:15:33 +0200

    added support for overriding params from command line, not integrated with simulation yet

---
+++ args.py
@@ -0,0 +1,73 @@
+from argparse import ArgumentParser, FileType
+from os.path import exists
+
+def create_parser():
+	parser = ArgumentParser()
+	parser.add_argument("--params",
+		type=str,
+		metavar="filename",
+		default="params.py",
+		help="default parameters filename")
+	parser.add_argument("--pop_size",
+		type=int,
+		metavar="integer",
+		help="population size")
+	parser.add_argument( "--mu",
+		type=float,
+		metavar="float",
+		help="mutation rate")
+	parser.add_argument( "--r",
+		type=float,
+		metavar="float",
+		help="recombination rate")
+	parser.add_argument( "--ticks",
+		type=int,
+		metavar="integer",
+		help="number of ticks")
+	parser.add_argument( "--num_loci",
+		type=int,
+		metavar="integer",
+		help="number of loci")
+	parser.add_argument( "--s",
+		type=float,
+		metavar="float",
+		help="selection coefficient")
+	parser.add_argument( "--debug",
+		type=bool,
+		metavar="boolean",
+		help="debug mode")
+	parser.add_argument( "--tick_interval",
+		type=int,
+		metavar="integer",
+		help="logging tick interval, 0 for no logging")
+	return parser
+
+
+def parse_args(parser):
+	args = parser.parse_args()
+	return args
+
+
+def load_params_file(filename):
+	params = {}
+	if filename and exists(filename):
+		fin = open(filename)
+		for line in fin:
+			k,v = line.split('=')
+			k,v = k.strip(), eval(v.strip())
+			params[k] = v
+		fin.close()
+	return params
+
+
+def args_and_params():	
+	args = parse_args(create_parser())
+	params = load_params_file(args.params)
+	args = vars(args)
+	args = { k: v for k,v in args.items() if v != None }
+	params.update(args)
+	globals().update(params)
+	print params
+
+if __name__ == '__main__':
+	args_and_params()
--- params.py
+++ params.py
@@ -1,5 +1,8 @@
-pop_size = 10 ** 6
+debug = True
+pop_size = 10 ** 1
 num_loci = 1000
 s = 0.01
 mu = 0.003
 r = 0.00006
+tick_interval = 0
+ticks = 1
