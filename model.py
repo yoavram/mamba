76f418d97b9a97632aec5c9e5c972dca3febf6da
Yoav Ram <yoavram+github@gmail.com>
2013-02-06 21:36:15 +0200

    comment - no beneficial mutations

--- model.py
+++ model.py
@@ -169,6 +169,7 @@ def mutation_recombination(population, genomes, mutation_rates, recombination_ra
 			if len(_loci) == 0:
 				continue
 			if method == 0: # mutation
+				# no beneficial mutations
 				new_alleles = (target_genome[_loci] + 1) % 2 # binomial(1, mutations.sum(), beta)
 			elif method == 1: # recombination
 				if rec_bar:
