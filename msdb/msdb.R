fe8ff310d7affd4f99ebed45f09fbe7b02b07bfb
Yoav Ram <yoavram+github@gmail.com>
2013-02-10 16:47:02 +0200

    nice visualization of first click dist

--- msdb/msdb.R
+++ msdb/msdb.R
@@ -22,9 +22,26 @@ for (filename in files) {
   data <- rbind(data, params)
 }
 
-df <- ddply(data, .(sumatra_label), transform, first.click=first.ratchet.click(jobname, sumatra_label))
-save(df, "msdb/dataframe.RData")
-p <- ggplot(df, aes(x=first.click, fill=pi+tau))
-p <- p + stat_bin() + facet_grid(facets=tau+r~pop_size, scales="free_y") + scale_fill_continuous()
-ggsave("msdb/histogram.pdf")
+plot.first.click.distr <- function(df, title='') {
+  q <- ggplot(data=df, mapping=aes(x=factor(pop_size), y=first.click))
+  q <- q + geom_boxplot(outlier.size=0, alpha=I(0))
+  q <- q + geom_jitter(aes(color=factor(pop_size)))
+  q <- q + facet_grid(facets=pi~tau, scale="free_y") 
+  q <- q +
+    xlab("Population size") + 
+    ylab("Time to first ratchet click") + 
+    ggtitle(paste("Tau/Pi", title)) +
+    scale_color_brewer(guide="none", palette="Set1") +
+    scale_y_log10()
+  return(q)
+}
 
+df <- ddply(data, .(sumatra_label), transform, first.click=first.ratchet.click(jobname, sumatra_label))
+save(df, file="msdb/no_beneficials.RData")
+
+r0 <- plot.first.click.distr(subset(df,pop_size!=5000 & r==0), "r=0")
+r0.003 <- plot.first.click.distr(subset(df,pop_size!=5000 & r==0.003), "r=0.003")
+
+pdf("msdb/no_beneficials.pdf",paper="a4")
+grid.arrange(r0,r0.003, ncol=1)
+dev.off()
