b2fcd868118ce6f7d68ba832983aefd84ebb6b9c
Yoav Ram <yoavram+github@gmail.com>
2013-05-20 15:02:43 +0300

    added mean time plot

--- invasion.summary.R
+++ invasion.summary.R
@@ -15,9 +15,9 @@ summary(df$final_tick)
 qplot(df$final_tick)
 qplot(df$in_final_rate)
 
-#q=ggplot(df,aes(x=r,y=in_final_rate,color=in_pi,group=in_pi))
-#q1=q+geom_jitter()+facet_grid(in_rho~in_phi)
-#q1
+q=ggplot(df,aes(x=r,y=final_tick,color=in_pi,group=in_pi))
+q1=q+geom_jitter()+facet_grid(in_rho~in_phi)
+q1
 
 agg.df <- ddply(df, .(in_phi,in_pi,in_tau,in_rho,r), summarize,
                 N = length(in_final_rate),
@@ -44,6 +44,20 @@ q7=q6+labs(x="recombination rate", y="mean invasion success")
 q8=q7+scale_fill_brewer(palette="Set1", name="invader", labels=c("CM","SIM","NM"))
 q8
 ggsave(filename=paste0(fname,'.pdf'), plot=q8, height=8.27, width=11.69)
+
+# GENERAL INVASION TIME PLOT
+q=ggplot(agg.df, aes(x=r,y=mean.tick, group=in_pi, ymin=mean.tick-2*se.tick, ymax=mean.tick+2*se.tick))
+q2=q + facet_grid(facets=in_rho~in_phi)
+q3=q2 + geom_bar(aes(fill=in_pi), stat="identity", position=dodge, width=0.9)
+q4=q3# + coord_cartesian(ylim=c(0-0.5,1-0.5))
+q5=q4+geom_errorbar(position=dodge, width=0.3)
+q6=q5#+scale_y_continuous(breaks=c(-0.5,-0.25,0,0.25,0.5), labels=c(0,0.25,0.5,0.75,1))
+q7=q6+labs(x="recombination rate", y="mean invasion time")
+q8=q7+scale_fill_brewer(palette="Set1", name="invader", labels=c("CM","SIM","NM"))
+q8
+
+ggsave(filename=paste0(fname,'_time.pdf'), plot=q8, height=8.27, width=11.69)
+
 
 # SIM ADVANTAGE WITH RECOMBINATION
 q=ggplot(subset(agg.df, in_phi=='SIR' & as.numeric(in_tau) <= 10), aes(x=r,y=mean.invasion-0.5, group=in_pi, ymin=mean.invasion-0.5-2*se.invasion,ymax=mean.invasion-0.5+2*se.invasion))
