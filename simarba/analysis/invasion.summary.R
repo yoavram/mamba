a6cb23fca357fe8168ff9ff7d35ccbab2b4edd70
Yoav Ram <yoavram@gmail.com>
2014-02-23 15:52:20 +0200

    SIM vs CM plot

--- simarba/analysis/invasion.summary.R
+++ simarba/analysis/invasion.summary.R
@@ -1,129 +1,46 @@
-library(ggplot2)
-library(plyr)
-library(scales)
+today = Sys.Date()
 
-theme_set(theme_gray(base_size = 26))
-setwd('D:\\workspace\\mamba\\simarba\\analysis')
+df1 = fread("invasion_summary_2013-11-17.csv")
+df2 = fread("invasionbig_summary_2013-11-20.csv")
+df2 = subset(df2, select=-c(adapt))
+df3 = fread("invasion_rb_summary_2013-11-19.csv")
+df3 = subset(df3, select=-c(adapt))
 
-fname <- "invasion_26_05_2013"
-df <- read.csv(paste0('..\\..\\output\\',fname,".csv.gz"))
-df <- df[df$r < 0.3,]
-df$in_tau <- factor(df$in_tau)
-df$in_tau <- factor(df$in_rho)
-df$r <- factor(df$r)
-df$in_phi[df$in_phi==1000] <- 'NR'
-df$in_phi[df$in_phi==1] <- 'SIR'
-df$in_phi[df$in_phi==0] <- 'CR'
-df$in_pi[df$in_pi==1000] <- 'NM'
-df$in_pi[df$in_pi==1] <- 'SIM'
-df$in_pi[df$in_pi==0] <- 'CM'
+dt = rbind(df1, df2, df3)
 
-summary(df$final_tick)
-qplot(df$final_tick)
-qplot(df$in_final_rate)
+dtt = dt[in_phi==1000 & beta<1 & rb==F & pop_size<1e8 & s==0.1, mean_se(in_final_rate), by="pi,tau,rho,phi,r,pop_size,envch_str,in_pi,in_tau,in_rho,in_phi,in_rate,beta,rb,mu,s,envch_start"]
+dim(dtt)
 
-q = ggplot(df, aes(x=r,y=final_tick,color=in_pi,group=in_pi))
-q1 = q + geom_jitter(alpha=1)+facet_grid(in_rho~in_phi)
-q2 = q1 + scale_fill_brewer(palette="Set1", name="invader")
-
-time.df <- ddply(df, .(in_phi,in_pi,in_tau,in_rho,r), summarize,
-                 N = length(in_final_rate),
-                 mean.tick = mean(final_tick, na.rm=T),
-                 se.tick = sd(final_tick, na.rm=T)/sqrt(length(final_tick)))
-
-q = ggplot(time.df, aes(x=r,y=mean.tick, group=in_pi))
-q1 = q + geom_bar(aes(fill=in_pi), stat="identity", position="dodge", width=0.9)
-q2 = q1 + facet_grid(in_rho~in_phi)
-q3 = q2 + scale_fill_brewer(palette="Set1", name="invader")
-
-#ggsave(plot=q3, filename=paste0(fname,'_time.pdf'), width=11,heigh=8)
-
-agg.df.all <- ddply(df, .(beta, mu, in_phi,in_pi,in_tau,in_rho,r, envch_str), summarize,
-                N = length(in_final_rate),
-                mean.tick = mean(final_tick, na.rm=T),
-                se.tick = sd(final_tick, na.rm=T)/sqrt(length(final_tick)),
-                mean.invasion = mean(in_final_rate,na.rm=T),                
-                se.invasion = sd(in_final_rate,na.rm=T)/sqrt(length(in_final_rate)))
-write.csv(agg.df.all, file=paste0(fname, '.csv'))
-
-agg.df <- subset(agg.df.all, beta < 1 & mu == 0.003 & envch_str == 4 & in_tau != 20 & in_tau != 1)
-
-q = ggplot(agg.df,aes(x=r,y=N, group=in_pi))
-q2 = q + facet_grid(facets=in_rho~in_phi)
-q3 = q2 + geom_bar(aes(fill=in_pi), stat="identity", position="dodge")
-q4 = q3 + scale_fill_brewer(palette="Set1", name="invader")
-q4 + labs(y="# samples") + theme(axis.text.x  = element_text(angle=45, vjust=0.5)) + 
-  scale_x_discrete(labels = function(x){format(as.numeric(x))})
-
-dodge <- position_dodge(width=0.9)
-
-# GENERAL INVASION SUCCESS PLOT
-q =ggplot(agg.df, aes(x=r,y=mean.invasion-0.5, group=in_pi, ymin=mean.invasion-0.5-2*se.invasion,ymax=mean.invasion-0.5+2*se.invasion))
-q2 = q + facet_grid(facets=in_rho~in_phi)#, labeller=function(var,val) {if (var=='in_rho') return(paste0('X',val)) else return(val)})
-q3 = q2 + geom_bar(aes(fill=in_pi), stat="identity", position=dodge, width=0.9)
-q4 = q3 + coord_cartesian(ylim=c(0-0.25,1-0.6))
-q5 = q4 + geom_errorbar(position=dodge, width=0.3)
-q6 = q5 + scale_y_continuous(breaks=c(-0.5,-0.25,0,0.25,0.5), labels=c(0,0.25,0.5,0.75,1))
-q7 = q6 + labs(x="recombination rate", y="mean invasion success")
-q8 = q7 + scale_fill_brewer(palette="Set1", name="invader")
-q9 = q8 + theme(axis.text.x  = element_text(angle=45, vjust=0.5)) 
-q10 = q9 + scale_x_discrete(labels = function(x){format(as.numeric(x), scientific=T)})
-q10
-
-ggsave(filename=paste0(fname,'.mean_invasion_Success.pdf'), plot=q10, height=8.27, width=11.69)
-
-# SIM ADVANTAGE WITH RECOMBINATION
-q=ggplot(subset(agg.df, beta<1 & in_pi!='NM' & in_phi=='NR' & as.numeric(in_tau) <= 10), aes(x=r,y=mean.invasion-0.5, group=in_pi, ymin=mean.invasion-0.5-2*se.invasion,ymax=mean.invasion-0.5+2*se.invasion))
-q2=q + facet_grid(facets=in_tau~.)#, labeller=function(var,val) {return(paste0('X',val))})
-q3=q2 + geom_bar(aes(fill=in_pi), stat="identity", position=dodge, width=0.9)
-q4=q3 + coord_cartesian(ylim=c(0-0.25,1-0.6))
-q5=q4 + geom_errorbar(position=dodge, width=0.3)
-q6=q5 + scale_y_continuous(breaks=c(-0.5,-0.25,0,0.25,0.5), labels=c(0,0.25,0.5,0.75,1))
-q7=q6 + labs(x="recombination rate", y="mean invasion success")
-q8=q7 + scale_fill_manual(values=c('#E41A1C', '#4DAF4A'), name="invader")
-q9 = q8 + scale_x_discrete(labels = function(x){format(as.numeric(x), scientific=T)})
-q9
-
-ggsave(filename=paste0(fname,'_SIM_with_r.tiff'), plot=q9, width=8.27, height=11.69, dpi=100)
-
-# beta=1
-agg.df.beta1 <- subset(agg.df.all, beta == 1 & in_tau != 1)
-unique(agg.df.beta1$beta)
-unique(agg.df.beta1$in_tau)
-unique(agg.df.beta1$r)
-unique(agg.df.beta1$mu)
-
-# GENERAL INVASION SUCCESS PLOT
-q=ggplot(agg.df.beta1, aes(x=r,y=mean.invasion-0.5, group=in_pi, ymin=mean.invasion-0.5-2*se.invasion,ymax=mean.invasion-0.5+2*se.invasion))
-q2=q + facet_grid(facets=in_rho~in_phi)
-q3=q2 + geom_bar(aes(fill=in_pi), stat="identity", position=dodge, width=0.9)
-q4=q3 + coord_cartesian(ylim=c(0-0.25,1-0.5))
-q5=q4+geom_errorbar(position=dodge, width=0.3)
-q6=q5+scale_y_continuous(breaks=c(-0.5,-0.25,0,0.25,0.5), labels=c(0,0.25,0.5,0.75,1))
-q7=q6+labs(x="recombination rate", y="mean invasion success")
-q8=q7+scale_fill_brewer(palette="Set1", name="invader")
-q9 = q8 + theme(axis.text.x  = element_text(angle=45, vjust=0.5)) 
-q10 = q9 + scale_x_discrete(labels = function(x){format(as.numeric(x), scientific=T)})
-q10
-
-ggsave(filename=paste0(fname,'_beta_1.pdf'), plot=q10, height=8.27, width=11.69)
+dtt[,r:=as.factor(r)]
+dtt[,pi:=factor(dtt$pi,levels=c(0,1,1000),labels=c("CM","SIM","NM"))]
+dtt[,tau:=as.factor(tau)]
+dtt[,in_pi:=factor(dtt$in_pi,levels=c(0,1,1000),labels=c("CM","SIM","NM"))]
+dtt[,in_tau:=as.factor(in_tau)]
+dtt[,pop_size:=as.factor(pop_size)]
 
 
-# SPECIFIC COMPARISON FOR LILACH'S TALK
+g = ggplot(data=dtt, mapping=aes(x=r,y=y)) + facet_grid(drop=T,facets=pop_size+envch_str~in_tau) +
+  labs(y="invader rate", x="recombination rate")
+g1 = g + geom_line(aes(color=in_pi,group=in_pi))
+g2 = g1  + geom_errorbar(aes(ymax=ymax,ymin=ymin), position="dodge", width=0.2) + 
+  scale_color_brewer("invader", palette="Set1") +
+  theme(axis.text.x = element_text(angle = 270, hjust = 1))
+g2
 
-df.lilach <- subset(agg.df.all, (in_tau==2 | in_tau==5 | in_tau==10) & envch_str==4 & beta<1)
-df.lilach <- subset(df.lilach, (in_pi=='SIM' & in_phi=='NR') | (in_pi=='SIM' & in_phi=='SIR') | (in_pi=='NM' & in_phi=='SIR'))
-df.lilach$invader <- factor(paste(df.lilach$in_pi, df.lilach$in_phi))
+ggsave(filename=paste0("invasion_",today,".png"), plot=g2)
 
-q = ggplot(df.lilach, aes(x=invader, y=mean.invasion-0.5, group=in_pi, ymin=mean.invasion-0.5-2*se.invasion,ymax=mean.invasion-0.5+2*se.invasion))
-q2 = q + facet_grid(facets=in_tau~r)
-q3 = q2 + geom_bar(aes(fill=invader), stat="identity", position=dodge, width=0.9)
-q4=q3 + coord_cartesian(ylim=c(0-0.25,1-0.5))
-q5=q4+geom_errorbar(position=dodge, width=0.3)
-q6=q5+scale_y_continuous(breaks=c(-0.5,-0.25,0,0.25), labels=c(0,0.25,0.5,0.75))
-q7=q6+labs(y="mean invasion success")
-q8=q7+scale_fill_manual(values=c('#FF7F00', '#4DAF4A', '#377EB8'))
-q9=q8+scale_x_discrete(name="",labels=c())
-q9
 
-ggsave(filename=paste0(fname, "_sim_sir.tiff"), plot=q9, height=8.27, width=11.69, dpi=100)
+g = ggplot(data=dtt[in_pi!="NM" & in_tau!=100 & pop_size==1e6 & envch_str==4], mapping=aes(x=r, y=y, ymin=ymin, ymax=ymax, group=in_pi)) + 
+  facet_grid(facets=in_tau~., labeller = label_bquote(tau == .(x))) +
+  scale_color_brewer("Invader", palette="Set1", guide = FALSE) +
+  theme(text = element_text(size=16), axis.text = element_text(size=11), axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x="Recombination rate", y="Fixation Probability\n") + 
+  geom_errorbar(aes(color=in_pi), size=0.5, width=0.2) + 
+  geom_line(aes(color=in_pi, linetype=in_pi), size=1) + 
+  geom_hline(y=0.5, color="black", linestyle="dashed") + 
+  scale_y_continuous(limits=c(0.1,0.9), breaks=c(0.25,0.5,0.75)) + 
+  scale_linetype_manual("Invader", values=c("dashed","solid"), guide = FALSE)
+g
+ggsave(filename=paste0("invasion_SIMvsCM_", today, ".png"), plot=g, width=4, height=6)
+
+
