c7024fc1fe909a3f58fc7c8d75e8326813567a86
Yoav Ram <yoavram@gmail.com>
2014-02-26 14:27:05 +0200

    fig 6 - adaptation time

--- simarba/analysis/adaptation.R
+++ simarba/analysis/adaptation.R
@@ -2,19 +2,29 @@ today = Sys.Date()
 
 dt = fread("adaptation_summary_2013-11-20.csv")
 
-dtt = dt[tau>1, mean_se(final_tick), by="pi,tau,r,pop_size,envch_str"]
+dtt = dt[, mean_se(final_tick), by="pi,tau,r,phi,rho,pop_size,envch_str"]
 
 dtt[,r:=as.factor(r)]
 dtt[,pi:=factor(dtt$pi,levels=c(0,1,1000),labels=c("CM","SIM","NM"))]
+dtt[,phi:=factor(dtt$phi,levels=c(0,1,1000),labels=c("CR","SIR","NR"))]
 dtt[,tau:=as.factor(tau)]
+dtt[,rho:=as.factor(rho)]
 dtt[,pop_size:=as.factor(pop_size)]
+dtt[,envch_str:=as.factor(envch_str)]
 
-g = ggplot(data=dtt,mapping=aes(x=r,y=y)) +
-  facet_grid(facets=pop_size+envch_str~tau, scales="free") +
-  labs(y="adaptation time", x="recombination rate") 
-  #scale_y_log10()
-g1 = g + geom_point(aes(color=pi,group=pi), size=2)  + scale_color_brewer("mutator", palette="Set1")
-g2 = g1 + geom_errorbar(aes(y=y,ymax=ymax, ymin=ymin, color=pi), width=0.1)
-g3 = g2 + geom_smooth(aes(group=pi,color=pi), method="lm", se=F, size=1)
-
-ggsave(plot=g3, filename=paste0("adaptation_",today,".png"))
+# Overview
+data = dtt[phi=="NR"]
+g = ggplot(data=data, mapping=aes(x=r, y=y, color=pi, group=interaction(pi,envch_str))) +
+  theme_bw() + 
+  geom_line(aes(linetype=pi), size=1) +   
+  geom_errorbar(aes(y=y,ymax=ymax, ymin=ymin, color=pi), width=0.3) + 
+  geom_point(aes(shape=envch_str), size=3) + 
+  facet_grid(facets=pop_size~tau, scales="free", labeller=tau_label) +
+  theme(text = element_text(size=16), axis.text = element_text(size=11), axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(y="Adaptation time\n", x="Recombination rate") + 
+  scale_color_brewer("Mutator", palette="Set1") +
+  #scale_y_log10(breaks=c(100,250,500,1000,2000,5000)) +
+  scale_linetype_manual("Mutator", values=c("dashed","solid","dotted")) + 
+  scale_shape_manual("# Mutaions", values=c(1,2))
+g
+ggsave(filename=paste0("adaptation_NR_",today,".png"), plot=g, width=7, height=6)
