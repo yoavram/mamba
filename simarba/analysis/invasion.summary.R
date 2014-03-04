f937293ed7f02d77d7d05b35546edfc0660e94bb
Yoav Ram <yoavram@gmail.com>
2014-03-04 15:54:59 +0200

    added heatmap version of fig 5

--- simarba/analysis/invasion.summary.R
+++ simarba/analysis/invasion.summary.R
@@ -24,6 +24,7 @@ tau_label = function(variable,value){
 
 
 today = Sys.Date()
+setwd("simarba/analysis/")
 
 df1 = fread("invasion_summary_2013-11-17.csv")
 df2 = fread("invasionbig_summary_2013-11-20.csv")
@@ -64,7 +65,7 @@ g
 ggsave(filename=paste0("invasion_SIMvsCM_pop_1e6_", today, ".png"), plot=g, width=4, height=6)
 
 # Figure 2: beta
-data=dtt[rb==F & in_phi=="NR" & in_pi!="NM" & in_tau!=100 & pop_size==1e5 & envch_str==4 & in_tau!=20]
+data=dtt[rb==F & r!=0.3 & in_phi=="NR" & in_pi!="NM" & in_tau!=100 & pop_size==1e5 & envch_str==4 & in_tau!=20]
 g = ggplot(mapping=aes(x=r, y=y, ymin=ymin, ymax=ymax, group=in_pi), data=data) + 
   theme_bw() +  
   facet_grid(facets=in_tau~beta, labeller = tau_label) +  
@@ -125,7 +126,24 @@ g = ggplot(mapping=aes(x=r, y=y, ymin=ymin, ymax=ymax, group=in_pi), data=data) 
 g = g +  scale_color_brewer("", palette="Set1") + #, guide = FALSE) +
   scale_linetype_manual("", values=c("dashed","solid","dotted"))#, guide = FALSE)
 g
-ggsave(filename=paste0("invasion_combined_tau_5_pop_sizes_", today, ".png"), plot=g, width=6, height=6)
+#ggsave(filename=paste0("invasion_combined_tau_5_pop_sizes_", today, ".png"), plot=g, width=6, height=6)
+
+
+#Figure 5b: recombinator+mutator
+data = dtt[rb==F & envch_str==4 & beta<1 & pop_size==1e6 & in_tau!=100]
+data$in_tau = as.numeric(levels(data$in_tau))[data$in_tau]
+g = ggplot(mapping=aes(x=in_tau, y=y, ymin=ymin, ymax=ymax, group=in_pi), data=data) + 
+  theme_bw() +
+  facet_grid(facets=r~in_phi, labeller = tau_label) +
+  theme(text = element_text(size=16), axis.text = element_text(size=11), axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x="Modifier effect", y="Fixation Probability\n") + 
+  geom_errorbar(aes(color=in_pi), size=0.5, width=0.2) + 
+  geom_line(aes(color=in_pi, linetype=in_pi), size=1) + 
+  geom_hline(y=0.5, color="black", linestyle="dashed") + 
+  scale_y_continuous(limits=c(0.0,1.0), breaks=c(0,0.25,0.5,0.75,1))
+g = g +  scale_color_brewer("", palette="Set1") + #, guide = FALSE) +
+  scale_linetype_manual("", values=c("dashed","solid","dotted"))#, guide = FALSE)
+g
 
 
 # Figure S1: pop size
@@ -159,3 +177,22 @@ g = g + scale_color_brewer("", palette="Set1") + #, guide = FALSE) +
   scale_linetype_manual("", values=c("dashed","solid", "dotted")) #, guide = FALSE)
 g
 #ggsave(filename=paste0("invasion_SIMvsCM_pop_1e6_", today, ".png"), plot=g, width=4, height=6)
+
+
+
+#Figure 5 - heatmap
+data = dtt[rb==F & envch_str==4 & beta<1 & in_tau!=100 & pop_size==1e6]
+g = ggplot(mapping=aes(x=in_tau, y=r), data=data) + 
+  theme_bw() +
+  facet_grid(facets=in_phi~in_pi, labeller = tau_label, as.table=F) +
+  theme(text = element_text(size=16), axis.text = element_text(size=11), axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x="Modifier Effect", y="Recombination Rate")  
+
+g = g + geom_tile(mapping=aes(fill=y)) + 
+  scale_fill_gradient2("Fixation\nProbability", midpoint=0.5, limits=c(0.0,1.0), breaks=c(0,0.5,1),
+                       low="darkred", mid="white", high="steelblue", na.value="black")
+                       #low="#ef8a62", mid="#f7f7f7", high="#67a9cf", na.value="gray50")
+ann_text <- data.frame(in_tau="5", r="0.003",
+                           in_pi="NM", in_phi="NR")
+g = g + geom_text(data=ann_text,label="\nControl", size=8, color="gray50")
+ggsave(filename=paste0("invasion_invasion_combined_heatmap_N_1e6_", today, ".png"), plot=g, width=7, height=6)
