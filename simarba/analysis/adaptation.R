93b26cfe6d112c0d0d7c46faa1a4c0030f258399
Yoav Ram <yoavram@gmail.com>
2014-03-10 15:35:50 +0200

    added adaptation with pop size 1e7 and the heatmap for fig 5

--- simarba/analysis/adaptation.R
+++ simarba/analysis/adaptation.R
@@ -1,6 +1,6 @@
 today = Sys.Date()
 
-dt = fread("adaptation_summary_2013-11-20.csv")
+dt = fread("adaptation_summary_2014-03-10.csv")
 
 dtt = dt[, mean_se(final_tick), by="pi,tau,r,phi,rho,pop_size,envch_str"]
 
@@ -21,10 +21,10 @@ g = ggplot(data=data, mapping=aes(x=r, y=y, color=pi, group=interaction(pi,envch
   geom_point(aes(shape=envch_str), size=3) + 
   facet_grid(facets=pop_size~tau, scales="free", labeller=tau_label) +
   theme(text = element_text(size=16), axis.text = element_text(size=11), axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(y="Adaptation time\n", x="Recombination rate") + 
-  scale_color_brewer("Mutator", palette="Set1") +
+  labs(y="Adaptation time\n", x="Recombination rate")
+g = g + scale_color_brewer("Mutator", palette="Set1") +
   #scale_y_log10(breaks=c(100,250,500,1000,2000,5000)) +
   scale_linetype_manual("Mutator", values=c("dashed","solid","dotted")) + 
-  scale_shape_manual("# Mutaions", values=c(1,2))
+  scale_shape_manual("# Mutaions", values=c(16,2))
 g
 ggsave(filename=paste0("adaptation_NR_",today,".png"), plot=g, width=7, height=6)
